27e81cb72fbd48f52e2b9781519449905a8cbc7f
edujimenezc <edujimenez010502@gmail.com>
2021-05-10 00:25:01 +0200

    -

--- ProyectoWeb/JS/alertaInicioSesion.js
+++ ProyectoWeb/JS/alertaInicioSesion.js
@@ -47,7 +47,7 @@ function validarXML() {
 		
 		}
 	};
-	xhr.open("GET", "https://carlosboniniklison.github.io/publico/ejercicios/xml/registrados.xml", true);
+	xhr.open("GET", "https://github.com/edujimenezc/Publico/blob/main/ProyectoWeb/XML/registrados.xml", true);
 	xhr.send();
 }
 
