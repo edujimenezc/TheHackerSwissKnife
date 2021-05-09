a6a14de2db33ab1d17ec2ed9d0f22b7d95617433
edujimenezc <edujimenez010502@gmail.com>
2021-05-09 16:38:06 +0200

    adicion de funciones js pero sigue el fallo del LocalStorage

--- ProyectoWeb/JS/alertaInicioSesion.js
+++ ProyectoWeb/JS/alertaInicioSesion.js
@@ -1,53 +1,3 @@
-let comprobado = false;
-var inicioSesionONo = function () {
-	//falta que se ponga true tio
-	if (comprobado == false) {
-		alerta();
-	} else {
-		console.log("no deberia de salir ")
-	}//ahora mostrar la parte oculta
-
-}
-
-var cambiarComprobado = function (valor) {
-	comprobado = valor;
-	return comprobado;
-}
-
-
-
-
-var alerta = function () {
-   
-    swal({
-        title: "¡Biemvenido!",
-        text: "¿Deseas iniciar sesión o seguir navegando como invitado?",
-        
-        buttons: {
-            //con go me lleva a iniciar sesión ,sino se cierra el alert
-            go: {
-                text: "Iniciar sesión",
-                value: "go",
-            },
-            cancel: "Continuar como invitado",
-        }
-    })
-    .then((value) => {
-      switch (value) {
-
-          case "go":
-              window.open("../ProyectoWeb/HTML/inicio-sesion.html","_self")
-              break;
-
-          
-      }
-  });
-            
-   
-    }
-
-
-
 
 
 
@@ -87,13 +37,15 @@ let nombre = getNombre();
 let pass = getPass();
 
 function validarXML() {
+	
 	console.log("Qui empieza la funcion del boni");
 	// lee desde aquí.
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function () {
 		if (this.readyState == 4 && this.status == 200) {
 			console.log("Qui empieza la funcion del boni2222");
-			miFuncion(this);
+			 miFuncion(this);
+		
 		}
 	};
 	xhr.open("GET", "https://carlosboniniklison.github.io/publico/ejercicios/xml/registrados.xml", true);
@@ -108,7 +60,8 @@ function miFuncion(xml) {
 	var xmlDoc = xml.responseXML;
 	var x = xmlDoc.getElementsByTagName("usuario");
 	
-
+	
+	
 	//var tabla = "<table><tr><th>Empleado</th><th>Clave</th></tr>";
 	for (i = 0; i < x.length; i++) {
 		// leo las etiquetas que me interesan del objeto
@@ -120,13 +73,21 @@ function miFuncion(xml) {
 
 		if (usrNom == getNombre() && usrPsw == getPass()) {
 			console.log("USUARIO CORRECTO");
-			cambiarComprobado(true);
-			console.log(comprobado);
+			
+			
 			alert("Usuario y contraseña correctos");
-			window.open("../index.html", "_self");
+			comp = true;
+			guardar_localStorage(true);
+			 validado = obtener_localStorage();
+			console.log(validado);
+			window.open("../index.html", "_self")
+		
+			
+						
 			break;
 		} else {
 			console.log("USUARIO INCORRECTO");
+			
 
 		}
 
@@ -144,11 +105,7 @@ function miFuncion(xml) {
 			console.log(datos);
 		});
 	});
-	if (logueado == false) {
-		alert("Usuario y contraseña incorrectos");
-
-	}
-
+	
 }
 
 
