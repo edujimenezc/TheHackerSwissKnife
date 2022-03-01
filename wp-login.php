b3b558ba43eba493223bfa3d8a6c08b0808d8131
edujimenezc <74304450+edujimenezc@users.noreply.github.com>
2022-03-01 18:26:45 +0100

    -

---
+++ wp-login.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Acceder 
+	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
+<link rel="dns-prefetch" href="//s.w.org">
+<link rel="stylesheet" id="dashicons-css" href="http://https://edujimenezc.github.io/Blog/wp-includes/css/dashicons.min.css?ver=5.9.1" media="all">
+<link rel="stylesheet" id="buttons-css" href="http://https://edujimenezc.github.io/Blog/wp-includes/css/buttons.min.css?ver=5.9.1" media="all">
+<link rel="stylesheet" id="forms-css" href="http://https://edujimenezc.github.io/Blog/wp-admin/css/forms.min.css?ver=5.9.1" media="all">
+<link rel="stylesheet" id="l10n-css" href="http://https://edujimenezc.github.io/Blog/wp-admin/css/l10n.min.css?ver=5.9.1" media="all">
+<link rel="stylesheet" id="login-css" href="http://https://edujimenezc.github.io/Blog/wp-admin/css/login.min.css?ver=5.9.1" media="all">
+	<meta name="referrer" content="strict-origin-when-cross-origin">
+		<meta name="viewport" content="width=device-width">
+		</title>
+</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-es-es">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
+		<div id="login">
+		<h1><a href="https://es.wordpress.org/">Funciona con WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="http://https://edujimenezc.github.io/Blog/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Nombre de usuario o correo electrónico</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off">
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Contraseña</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20">
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Mostrar la contraseña">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Recuérdame</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Acceder">
+									<input type="hidden" name="redirect_to" value="http://https://edujimenezc.github.io/Blog/wp-admin/">
+									<input type="hidden" name="testcookie" value="1">
+			</p>
+		</form>
+
+					<p id="nav">
+								<a href="http://https://edujimenezc.github.io/Blog/wp-login.php?action=lostpassword">¿Has olvidado tu contraseña?</a>
+			</p>
+					<script type="text/javascript">
+			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
+				<p id="backtoblog">
+			<a href="http://https://edujimenezc.github.io/Blog/">&larr; Ir a Eduardo Jiménez Cobos</a>		</p>
+			</div>
+				<div class="language-switcher">
+				<form id="language-switcher" action="" method="get">
+
+					<label for="language-switcher-locales">
+						<span class="dashicons dashicons-translation" aria-hidden="true"></span>
+						<span class="screen-reader-text">Idioma</span>
+					</label>
+
+					<select name="wp_lang" id="language-switcher-locales"><option value="en_US" lang="en" data-installed="1">English (United States)</option>
+<option value="es_ES" lang="es" selected data-installed="1">Español</option></select>
+					
+					
+					
+						<input type="submit" class="button" value="Cambiar">
+
+					</form>
+				</div>
+				<script src="http://https://edujimenezc.github.io/Blog/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
+<script src="http://https://edujimenezc.github.io/Blog/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+<script id="zxcvbn-async-js-extra">
+var _zxcvbnSettings = {"src":"http:\/\/https:\/\/edujimenezc.github.io\/Blog\/wp-includes\/js\/zxcvbn.min.js"};
+</script>
+<script src="http://https://edujimenezc.github.io/Blog/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script src="http://https://edujimenezc.github.io/Blog/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9" id="regenerator-runtime-js"></script>
+<script src="http://https://edujimenezc.github.io/Blog/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script src="http://https://edujimenezc.github.io/Blog/wp-includes/js/dist/hooks.min.js?ver=1e58c8c5a32b2e97491080c5b10dc71c" id="wp-hooks-js"></script>
+<script src="http://https://edujimenezc.github.io/Blog/wp-includes/js/dist/i18n.min.js?ver=30fcecb428a0e8383d3776bcdd3a7834" id="wp-i18n-js"></script>
+<script id="wp-i18n-js-after">
+wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
+</script>
+<script id="password-strength-meter-js-extra">
+var pwsL10n = {"unknown":"Fortaleza de la contraseña desconocida","short":"Muy débil","bad":"Débil","good":"Medio","strong":"Fuerte","mismatch":"No coinciden"};
+</script>
+<script id="password-strength-meter-js-translations">
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2022-02-22 09:13:24+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"es"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["¡%1$s está obsoleto desde la versión %2$s! Usa %3$s en su lugar. Por favor, plantéate escribir un código más inclusivo."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
+</script>
+<script src="http://https://edujimenezc.github.io/Blog/wp-admin/js/password-strength-meter.min.js?ver=5.9.1" id="password-strength-meter-js"></script>
+<script src="http://https://edujimenezc.github.io/Blog/wp-includes/js/underscore.min.js?ver=1.13.1" id="underscore-js"></script>
+<script id="wp-util-js-extra">
+var _wpUtilSettings = {"ajax":{"url":"\/blog\/wp-admin\/admin-ajax.php"}};
+</script>
+<script src="http://https://edujimenezc.github.io/Blog/wp-includes/js/wp-util.min.js?ver=5.9.1" id="wp-util-js"></script>
+<script id="user-profile-js-extra">
+var userProfileL10n = {"user_id":"0","nonce":"3b2da17435"};
+</script>
+<script id="user-profile-js-translations">
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2022-02-22 09:13:24+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"es"},"Your new password has not been saved.":["No ha sido guardada tu nueva contraseña."],"Hide":["Ocultar"],"Show":["Mostrar"],"Confirm use of weak password":["Confirma el uso de una contraseña débil."],"Hide password":["Ocultar la contraseña"],"Show password":["Mostrar la contraseña"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
+</script>
+<script src="http://https://edujimenezc.github.io/Blog/wp-admin/js/user-profile.min.js?ver=5.9.1" id="user-profile-js"></script>
+	<div class="clear"></div>
+	</body>
+	</html>
