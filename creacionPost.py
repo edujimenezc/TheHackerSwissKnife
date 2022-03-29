c80d6000dce8022403bcc4cb37551e9e85fce92f
edujimenezc <74304450+edujimenezc@users.noreply.github.com>
2022-03-29 15:42:42 +0200

    actualizacion

--- creacionPost.py
+++ creacionPost.py
@@ -1,51 +1,38 @@
 from tkinter.ttk import Style
 from bs4 import BeautifulSoup, NavigableString
-with open('w.html','r') as f:
+with open('index.html','r', encoding='utf-8') as f:
     lines = f.read()#.split(">")
 
 soup = BeautifulSoup( lines , 'html.parser')
 mydiv=soup.find("div", class_="wp-block-column")#pillo el div contenedor de divs
 
-
-#le meto el p
-new_tag_p = soup.new_tag("p", class_="has-text-align-center")
-titulo = NavigableString("Hello!")
-new_tag_p.insert(0,titulo)
-#div del contenido
 new_tag_div_grande = soup.new_tag("div", class_="wp-block-columns")
 #div con el texto
-new_tag_div_texto = soup.new_tag("div", class_="wp-block-column")
-#creo el p y le meto texto y estilo
-new_tag_texto=soup.new_tag("p")
-texto = NavigableString("texttt")
-new_tag_texto.insert(0,texto)
-new_tag_style_texto=soup.new_tag('style', type='text/css')
-new_tag_texto.append(new_tag_style_texto)
-new_tag_texto.style.append('flex-basis:66.66%')
-
-#meto el p en el div texto
-new_tag_div_texto.append(new_tag_texto)
-
-
-
-
-
-#creo el div de la foto
-new_tag_div_img = soup.new_tag("div", class_="wp-block-column")
-
-
-
-
-
-
-#meto el div texto en el div grande
-new_tag_div_grande.append(new_tag_div_texto)
+titulo='''
+<p class="has-text-align-center"><strong><mark class="has-inline-color has-vivid-green-cyan-color" style="background-color:rgba(0, 0, 0, 0)">{}</mark></strong></p>
+'''.format(input("Escribe el titulo\n"))
+new_tag_div_grande.append(BeautifulSoup(titulo, 'html.parser'))
+texto=input("Escribe el texto\n")
+enlace=input("Escribe el enlace\n")
+img=input("Escribe el nombre de la imagen\n")
+div= '''
+<div class="wp-block-columns">
+<div class="wp-block-column" style="flex-basis:66.66%">
+<p>{}</p>
+<p>Learn how I did it <a href="{}" rel="noreferrer noopener" target="_blank"><strong>Here</strong> </a>!</p>
+</div>
+<div class="wp-block-column" style="flex-basis:33.33%">
+<figure class="wp-block-image size-large"><img alt="" class="wp-image-112" height="576" sizes="(max-width: 1024px) 100vw, 1024px" src="{}" width="1024"></figure>
+</div>
+</div>
+'''.format(texto,enlace,img)
+new_tag_div_grande.append(BeautifulSoup(div, 'html.parser'))
 
 # le append el nuevo p
-mydiv.append(new_tag_p)
+
 mydiv.append(new_tag_div_grande)
 
 
-with open("output1.html", "w", encoding='utf-8') as file:
+with open("index.html", "w", encoding='utf-8') as file:
     file.write(str(soup))
 
